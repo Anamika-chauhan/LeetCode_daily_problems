e3a1987aeccff67bdd8b61f6de6137a948623543
Anamika-chauhan <83940146+Anamika-chauhan@users.noreply.github.com>
2022-11-26 19:38:48 +0530

    Time: 193 ms (85.50%), Space: 54.5 MB (74.35%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
-    // linear search 
-    
+    // binary search 
+
+    int floor(vector<vector<int>>& et, int val){
+        
+        int l=0, h=et.size()-1, ans=-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(et[mid][0] <= val){
+                ans=mid;
+                l=mid+1;
+            }
+            else
+                h=mid-1;
+        }
+        
+        return ans;
+    }
+
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         
         int n=startTime.size();
@@ -22,12 +38,10 @@ public:
             
             dp[i]=values[i][2];
             dp[i]=max(dp[i], dp[i-1]);
-            int temp=-1;
-            for(int j=i-1;j>=0;j--){
-                if(values[j][0] <= values[i][1]){    // endTime of j <= startTime of i
-                    dp[i]=max(dp[i], values[i][2]+dp[j]);
-                    break;
-                }
+            
+            int temp=floor(values, values[i][1]);
+            if(temp!=-1){
+                dp[i]=max(dp[i], values[i][2]+dp[temp]);
             }
             
             ans=max(ans, dp[i]);
