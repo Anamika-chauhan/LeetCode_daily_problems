360ac711ee80a9c3c68d205fa7d36617df873dfa
Anamika-chauhan <83940146+Anamika-chauhan@users.noreply.github.com>
2022-11-28 18:31:43 +0530

    Time: 291 ms (78.04%), Space: 60.5 MB (95.18%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
+    
     int minSetSize(vector<int>& arr) {
         
         int ans=0;
-        unordered_map<int, int>mp;
-        vector<int>v;
+        int mx=*max_element(arr.begin(), arr.end());
+        
+        vector<int>mp(mx+1, 0);
         
         for(int i=0;i<arr.size();i++)
             mp[arr[i]]++;
         
-        for(auto e:mp)
-            v.push_back(e.second);
-        
-        sort(v.begin(), v.end());
+        sort(mp.begin(), mp.end());
         int sum=0;
-      
-        for(int i=v.size()-1;i>=0;i--){
-            ans++;
-            sum+=v[i];
-            if(sum >= (arr.size()/2))
-                break;
-            
-        }
+        int sz=arr.size()/2;
         
+        for(int i=mp.size()-1;i>=0;i--){
+            if(mp[i]>=1){
+                ans++;
+                sum+=mp[i];   
+            }
+            if(sum >= sz)
+                break;
+        }      
+
         return ans;
     }
 };
