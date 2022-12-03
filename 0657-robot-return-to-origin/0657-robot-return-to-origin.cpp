e9a2272693472030e539dc388e9552af141308d4
Anamika-chauhan <83940146+Anamika-chauhan@users.noreply.github.com>
2022-12-03 21:55:35 +0530

    Time: 35 ms (15.33%), Space: 8.1 MB (44.25%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -3,11 +3,12 @@ public:
     bool judgeCircle(string moves) {
         
         int x=0, y=0;
+ 
         for(auto e:moves){
             if(e=='U') y+=1;
-            else if(e=='D') y-=1;
-            else if(e=='L') x-=1;
-            else    x+=1;
+            if(e=='D') y-=1;
+            if(e=='L') x-=1;
+            if(e=='R') x+=1;
         }
         
         return x==0 && y==0;
